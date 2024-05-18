0754e4770bbda4d5adfb853f53d01c27ed8813a2
sbeknur <sbeknur2003@gmail.com>
2024-05-19 03:13:33 +0500

    API updated

--- routes/places.js
+++ routes/places.js
@@ -4,13 +4,13 @@ import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 //CREATE
-router.post("/:hotelid", verifyAdmin, createPlace);
+router.post("/:restaurantid", verifyAdmin, createPlace);
 
 //UPDATE
 router.put("/availability/:id", updatePlaceAvailability);
 router.put("/:id", verifyAdmin, updatePlace);
 //DELETE
-router.delete("/:id/:hotelid", verifyAdmin, deletePlace);
+router.delete("/:id/:restaurantid", verifyAdmin, deletePlace);
 //GET
 
 router.get("/:id", getPlace);
