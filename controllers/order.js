42eb4dc03611aee7e2f244c3ef9ed22dbdd6669f
sbeknur <sbeknur2003@gmail.com>
2024-05-29 09:45:27 +0500

    fixed

--- controllers/order.js
+++ controllers/order.js
@@ -76,7 +76,7 @@ export const getOrdersByUser = async (req, res, next) => {
         const orders = await Promise.all(ordersPromises);
         user.orders = orders;
 
-        detailedOrders = user.orders;
+        const detailedOrders = user.orders;
 
         res.status(200).json(detailedOrders);
     } catch (err) {
